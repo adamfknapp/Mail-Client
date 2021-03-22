159b04f5b5c62f5f03ec7bc245755fb0d2eb4a93
Adam Knapp <adamfknapp@gmail.com>
2021-03-21 22:34:13 -0400

    SINGLE VIEW

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -9,8 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
   //create new email
   document.querySelector('#sendNewMail').addEventListener('click', send_new_mail);
 
-  // By default, load the inbox
-  //load_mailbox('inbox');
 });
 
 
@@ -19,8 +17,9 @@ function send_new_mail() {
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
-
+  
   // perform basic validation before POST to prescreen simple errors
+  // this could be moved to views.py to centralize the logic. 
   var completeSend = true
   if (subject.length === 0) {
       if (confirm('Send without a subject?')){
@@ -35,7 +34,6 @@ function send_new_mail() {
         completeSend = false
       }
   }
-  console.log(`Complete Send: ${completeSend}`)
  
   // Send mail if valid
   if (completeSend) {
@@ -59,10 +57,12 @@ function send_new_mail() {
   } 
 }
 
+
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none'
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -75,6 +75,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#single-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
@@ -85,6 +86,67 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(emails=>{
     console.log(emails)
- 
-  }) 
+    if (emails.length === 0) {
+      document.querySelector('#emails-view').innerHTML += 'No email found.';
+    }
+    emails.forEach(add_email_to_view);
+  });
+}
+
+function add_email_to_view(email){
+
+  const row = document.createElement('div');
+  row.classList.add('email-entry');
+  
+
+  if (email.read === true) {
+    row.classList.add('email-read');
+}
+
+  row.innerHTML = `${email.id} <span class='email-sender'>${email.sender}</span>  <span class='email-subject'>${email.subject}</span>  <span class='email-timestamp'>${email.timestamp}</span>`;
+
+  //On email click
+  row.addEventListener('click', function() {
+    single_email_view(email.id);  
+  });
+
+  document.querySelector('#emails-view').append(row);
+}
+
+function single_email_view(id){
+  // Show the single email view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+    
+  //Get the data
+  fetch(`emails/${id}`)
+  .then(response => response.json())
+  .then(email=>{
+    add_buttons(email)
+  })
+}
+
+function add_buttons(email) {
+  //reply button
+  const reply_button = document.createElement('button'); 
+  reply_button.innerHTML='Reply'; 
+  reply_button.addEventListener('click', function() {
+      console.log('Reply button clicked')
+    });
+
+  //Archive or unarchive button
+  const archive_toggle = document.createElement('button'); 
+  if (email.archived === true){
+    archive_toggle.innerHTML='Unarchive'
+  } else {
+    archive_toggle.innerHTML='Archive'
+  } 
+  
+  archive_toggle.addEventListener('click', function() {
+      console.log('Archive/Unarchive button clicked')
+    });
+
+  document.querySelector('#single-email-view').append(reply_button);
+  document.querySelector('#single-email-view').append(archive_toggle); 
 }
